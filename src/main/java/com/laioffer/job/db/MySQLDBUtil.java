082387bc83473fc69e9d551b06e08db150122e7e
Jingtian Zhang <jtz9317@gmail.com>
2020-11-12 21:46:44 -0800

    not working right now

--- src/main/java/com/laioffer/job/db/MySQLDBUtil.java
+++ src/main/java/com/laioffer/job/db/MySQLDBUtil.java
@@ -1,11 +1,11 @@
 package com.laioffer.job.db;
 
 public class MySQLDBUtil {
-    private static final String INSTANCE = YOUR_INSTANCE_ADDRESS;
+    private static final String INSTANCE = "laiproject-instance.crr1pvlw6sjd.us-west-2.rds.amazonaws.com";
     private static final String PORT_NUM = "3306";
-    public static final String DB_NAME = YOUR_DB_NAME;
+    public static final String DB_NAME = "laiproject";
     private static final String USERNAME = "admin";
-    private static final String PASSWORD = YOUR_PASSWORD;
+    private static final String PASSWORD = "zjtsam931124";
     public static final String URL = "jdbc:mysql://"
             + INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
             + "?user=" + USERNAME + "&password=" + PASSWORD
